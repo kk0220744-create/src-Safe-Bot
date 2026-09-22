67ce1e353836cef8738b860085ab8594b5cf7de4
kk0220744-create <kk0220744@gmail.com>
2026-09-22 20:05:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,20 +2,20 @@
 # Note if you are trying to deploy on vps then directly fill values in (""
 import os
 
-API_HASH = "f150646c78f09b4f88bef191a22539c0"
-API_ID = 26704085
-BOT_TOKEN = "8564747078:AAF4rw9Fw7sBI-NCdCZgOvaUL2mIGrleIss"
-CHANNEL_ID = -1003818713803
-LOG_GROUP = -1005121908460
-CLONE_LOG_CHANNEL = -1003886456761
+API_HASH = "1b3fd2a65ece10471d24b90e9ae3163e"
+API_ID = 39712114
+BOT_TOKEN = "8672146337:AAFq-9xHqBL1CSWI0mJnKlfNNGtkIbCgLzk"
+CHANNEL_ID = -1004299794278
+LOG_GROUP = -1004448076989
+CLONE_LOG_CHANNEL = -1004434809417
 # Keep this channel private and add only admins/lifetime members.
 PREMIUM_ARCHIVE_CHANNEL = int(os.environ.get("PREMIUM_ARCHIVE_CHANNEL") or CLONE_LOG_CHANNEL)
 # Public channel where cloned media is also posted so users get an
 # MX Player / VLC streamable link (t.me/Link09660/MSGID)
-STREAM_CHANNEL = -1004332477626
-STREAM_CHANNEL_USERNAME = "Link09660"
+STREAM_CHANNEL = -1004398941102
+STREAM_CHANNEL_USERNAME = "streamsafebro"
 # Owner(s) of the bot. Keep as a list for filters.user compatibility.
-OWNER_ID = [8552899459]
+OWNER_ID = [6749017641]
 
 # MongoDB removed from this codebase; keep MONGO_DB for backwards compatibility
-MONGO_DB = ""
+MONGO_DB = "mongodb+srv://TeleFlow:dbuserpassword.m@cluster0.er99jx9.mongodb.net/?appName=Cluster0"
